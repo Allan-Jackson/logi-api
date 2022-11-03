ef783d10eb80e898c7348c82d4038b14a8be9611
allanx <allan.jackson.programmer@gmail.com>
2022-11-03 01:14:57 -0300

    Get Cliente list using EntityManager

--- src/main/java/br/com/allanx/logiapi/api/controller/ClienteController.java
+++ src/main/java/br/com/allanx/logiapi/api/controller/ClienteController.java
@@ -5,26 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 
+    @PersistenceContext
+    EntityManager manager;
+
     @GetMapping
     public List<Cliente> listar() {
-        var cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setEmail("joao.example@mail.com");
-        cliente1.setTelefone("11 9999-9999");
-        cliente1.setNome("Joãozin Examplão");
-
-        var cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setEmail("maria.example@mail.com");
-        cliente2.setTelefone("11 9899-9999");
-        cliente2.setNome("Maria Examplão");
-
-        return List.of(cliente1, cliente2);
+        return manager.createQuery("from clientes", Cliente.class)
+                .getResultList();
     }
 }
--- src/main/java/br/com/allanx/logiapi/domain/model/Cliente.java
+++ src/main/java/br/com/allanx/logiapi/domain/model/Cliente.java
@@ -1,9 +1,21 @@
 package br.com.allanx.logiapi.domain.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity(name = "clientes")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
     private String nome;
     private String email;
