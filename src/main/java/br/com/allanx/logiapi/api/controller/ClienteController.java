71de44ed10eea34fc15b879a76079960c70c8afb
allanx <allan.jackson.programmer@gmail.com>
2022-11-03 22:41:57 -0300

    Alter ClienteController to use Lombok to generate the constructor

--- src/main/java/br/com/allanx/logiapi/api/controller/ClienteController.java
+++ src/main/java/br/com/allanx/logiapi/api/controller/ClienteController.java
@@ -2,6 +2,7 @@ package br.com.allanx.logiapi.api.controller;
 
 import br.com.allanx.logiapi.domain.model.Cliente;
 import br.com.allanx.logiapi.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
+@AllArgsConstructor //Lombok generates the constructor using all class fields (ClienteRepository)
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 
-    @Autowired
-    ClienteRepository repository;
+    private ClienteRepository repository;
 
     @GetMapping
     public List<Cliente> listar() {
