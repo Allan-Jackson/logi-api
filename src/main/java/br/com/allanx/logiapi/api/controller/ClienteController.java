e1461000ce7538a07ddd915015c627d284e73fa0
allanx <allan.jackson.programmer@gmail.com>
2022-11-03 23:43:34 -0300

    Add implementation for http methods PUT, POST and DELETE

--- src/main/java/br/com/allanx/logiapi/api/controller/ClienteController.java
+++ src/main/java/br/com/allanx/logiapi/api/controller/ClienteController.java
@@ -4,13 +4,15 @@ import br.com.allanx.logiapi.domain.model.Cliente;
 import br.com.allanx.logiapi.domain.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor //Lombok generates the constructor using all class fields (ClienteRepository)
 @RestController
@@ -22,5 +24,38 @@ public class ClienteController {
     @GetMapping
     public List<Cliente> listar() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscar(@PathVariable(name = "id") Long clienteId) {
+        Optional<Cliente> cliente = repository.findById(clienteId);
+        if(cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable("id") Long clienteId, @RequestBody Cliente cliente) {
+        if(repository.existsById(clienteId)) {
+            cliente.setId(clienteId);
+            return ResponseEntity.ok(repository.save(cliente));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> remover(@PathVariable("id") Long clienteId) {
+        if(repository.existsById(clienteId)) {
+            repository.deleteById(clienteId);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
