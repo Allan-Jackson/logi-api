f860eb540caeee21dc897032760ad97335d06b48
allanx <allan.jackson.programmer@gmail.com>
2022-11-07 16:16:26 -0300

    Add global exception handler

---
+++ src/main/java/br/com/allanx/logiapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,37 @@
+package br.com.allanx.logiapi.api.exceptionhandler;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<Problema.Campo> campos = new ArrayList<>();
+
+        for(ObjectError erro : ex.getBindingResult().getAllErrors()) {
+            String nome = ((FieldError) erro).getField();
+            String mensagem = erro.getDefaultMessage();
+            campos.add(new Problema.Campo(nome, mensagem));
+        }
+
+        Problema problema = new Problema();
+        problema.setStatus(status.value());
+        problema.setDataHora(LocalDateTime.now());
+        problema.setTitulo("Um ou mais campos inválidos. Faça o preenchimento corretamente e tente novamente.");
+        problema.setCampos(campos);
+
+        return handleExceptionInternal(ex, problema, headers, status, request);
+    }
+}
